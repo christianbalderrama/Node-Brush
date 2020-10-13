500c059c9603843c26e6b9afb901fa25b66e0b69
Christian Balderrama <0christianbalderrama@gmail.com>
2020-10-13 20:46:49 +0800

    :pencil: Update comment

--- exports/2. modular-exports.js
+++ exports/2. modular-exports.js
@@ -1,7 +1,17 @@
 const {
   porsche,
   ferrari,
+  ...rest
 } = require('./files/lib');
 
+/**
+ * Here, we are trying to import things when we want to, there's still other
+ * stuffs inside the file but we don't get them.
+ *
+ * Logging the `rest` using the spread operator then shows the other car
+ * manufacturer informations inside the file which is also exported.
+ * @type {String}
+ */
 console.log('Ferrari: ', ferrari);
 console.log('Porsche: ', porsche);
+console.log('Rest: ', rest);
