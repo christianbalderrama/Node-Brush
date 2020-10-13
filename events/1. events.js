0ed04d57e911f3f5c6e82aa1ab975cada889055f
Christian Balderrama <0christianbalderrama@gmail.com>
2020-10-13 20:49:36 +0800

    :pencil: Add comment to events

--- events/1. events.js
+++ events/1. events.js
@@ -1,8 +1,17 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
+/**
+ * We declare an event called `add` that if there's an event
+ * fired name `add` we execute this callback/function that has
+ * 2 parameters which is x and y that returns the sum of numbers.
+ */
 eventEmitter.on('add', (x, y) => {
   return console.log(x + y);
 });
 
+/**
+ * We emit an event named `add` using the `emit` keyword from the
+ * `eventEmitter` which executes the function above.
+ */
 eventEmitter.emit('add', 1, 2);
